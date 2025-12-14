fd99c88db8c5d7ca114b63d4e516122a00c3861d
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-12-14 08:05:05 -0300

    fea: changed the main page to not render UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Manager Panel
-    </div>
-  );
+"use client";
+
+import { useAuth } from "@/context/auth-context";
+import { redirect } from "next/navigation";
+
+export default function RootPage() {
+  const { user } = useAuth();
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  const barbershopId = user.barbershopId?.[0];
+
+  redirect(`/barbershop/${barbershopId}/home`);
 }
