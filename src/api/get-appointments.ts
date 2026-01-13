7af82d4aeb2748a675ae9f1d6b701bc960bb48c8
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-01-12 22:49:34 -0300

    feat: create get appointment api call

---
+++ src/api/get-appointments.ts
@@ -0,0 +1,29 @@
+import { httpRequest } from "../config/axios/httpRequest";
+import { Appointment } from "../config/entities/appointments/appointments.types";
+import { generateReactQuery } from "../helpers/react-query";
+
+export const GET_APPOINTMENTS_BY_BARBERSHOP_QUERY_KEY =
+  "GET_APPOINTMENTS_BY_BARBERSHOP_QUERY_KEY";
+
+interface GetAppointmentsByBarbershopParams {
+  barbershopId: string;
+}
+
+export const getAppointmentsByBarbershop = async ({
+  barbershopId,
+}: GetAppointmentsByBarbershopParams): Promise<Appointment[]> => {
+  const path = `/appointment/barbershop/${barbershopId}`;
+
+  try {
+    const response = await httpRequest.get<Appointment[]>(path);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching appointments:", error);
+    throw error;
+  }
+};
+
+export const useGetAppointmentsByBarbershop = generateReactQuery<
+  Appointment[],
+  GetAppointmentsByBarbershopParams
+>(GET_APPOINTMENTS_BY_BARBERSHOP_QUERY_KEY, getAppointmentsByBarbershop);
