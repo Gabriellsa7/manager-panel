e7ea2acf12b7dacc6db5ae2f07026a796a2a22ce
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-01-19 11:16:43 -0300

    feat: create the signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,103 @@
+"use client";
+import { useCreateUser } from "@/src/api/post-create-user";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const { mutateAsync, isPending } = useCreateUser();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await mutateAsync({
+        name,
+        email,
+        password,
+      });
+
+      router.replace("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
-      <h1 className="text-2xl font-semibold">Signup Page</h1>
+    <div
+      className="flex min-h-screen items-center justify-center 
+  bg-linear-to-b from-zinc-950 via-black to-purple-900"
+    >
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-sm rounded-xl 
+  bg-black/60 backdrop-blur 
+  p-6 shadow-xl border border-white/10"
+      >
+        <h1 className="mb-6 text-center text-xl font-semibold text-white">
+          Login
+        </h1>
+
+        <div className="mb-4">
+          <label className="mb-1 block text-sm text-white">Name</label>
+          <input
+            type="name"
+            placeholder="Enter Your Name"
+            className="w-full rounded border-none bg-white p-2 text-black placeholder:text-gray-600 placeholder:text-xs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="mb-1 block text-sm text-white">Email</label>
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            className="w-full rounded border-none bg-white p-2 text-black placeholder:text-gray-600 placeholder:text-xs"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="mb-1 block text-sm text-white">Password</label>
+          <input
+            type="password"
+            placeholder="Enter Your Password"
+            className="w-full rounded border-none bg-white p-2 text-black placeholder:text-gray-600 placeholder:text-xs"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-6">
+          <button
+            type="submit"
+            disabled={isPending}
+            className="w-full rounded-lg bg-[#8162FF] py-2 text-white disabled:opacity-60"
+          >
+            {isPending ? "Creating..." : "Signup"}
+          </button>
+          <div className="flex justify-center">
+            <span className="text-white text-sm">
+              Already have an account?{" "}
+              <button
+                onClick={() => router.replace("/login")}
+                className="text-[#8162FF] underline cursor-pointer"
+              >
+                Signin
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
